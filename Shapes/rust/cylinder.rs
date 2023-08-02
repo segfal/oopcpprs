d7d731f7353c56ae584cc36606c45533975a6649
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-08-02 18:27:53 -0400

    few mods

---
+++ Shapes/rust/cylinder.rs
@@ -0,0 +1,10 @@
+// File: cylinder.rs
+
+pub struct Cylinder {
+    x: i32, // Center coordinates
+    y: i32,
+    radius: i32, // Cylinder radius
+    height: i32, // Cylinder height
+    color: i32,
+    id: i32,
+}
--- main.rs
+++ main.rs
@@ -1,5 +1,7 @@
 
 
+
 fn main(){
     println!("Hello, world!");
 }
+
